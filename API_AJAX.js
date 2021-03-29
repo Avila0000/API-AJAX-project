8ec76367b086ecbe867df5a5455a0bad41de5d2a
Leiver Avila <leiveravila@Leivers-Air.attlocal.net>
2021-03-28 19:24:27 -0500

    few minor changes

---
+++ API_AJAX.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.random').click(function(){
+        console.log('It Clicked!')
+        let movieNum = Math.floor((Math.random() * 20) +1);
+        console.log('finally!')
+        $.get("https://ghibliapi.herokuapp.com/films" , function(res){
+                console.log(res)
+        $('.movies').html(
+            `<tr>
+                <th>Movie Title</th>
+                <th>Original Title</th>
+                <th>Director</th>
+                <th>Description</th>
+                <th>Release Date</th>
+            </tr>
+            <tr>
+                <td>${res[movieNum].title}</td>
+                <td>${res[movieNum].original_title}</td>
+                <td>${res[movieNum].director}</td>
+                <td>${res[movieNum].description}</td>
+                <td>${res[movieNum].release_date}</td>
+            </tr>`
+            )
+        })
+    })
+    $('.clear').click(function(){
+        $('.movies').html('')
+        console.log('CLEARED!')
+    })
+})
